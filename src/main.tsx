1cd557d52cd3aa5889409f55c6b1d8033145ae45
OctupusPrime <rasiapypkin2014@gmail.com>
2023-05-21 14:28:13 +0300

    fix build

--- src/main.tsx
+++ src/main.tsx
@@ -2,19 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-import { AuthContextProvider, useAuthContext } from "@context/AuthContext.tsx";
+import { AuthContextProvider } from "@context/AuthContext.tsx";
 import QueryContextProvider from "@context/QueryContext.tsx";
 import { RouterProvider } from "@tanstack/router";
 import { queryClient, router } from "./router.tsx";
 
 import { MantineProvider } from "@mantine/core";
 
-const App = () => {
-  const { isLoading } = useAuthContext();
+// const App = () => {
+//   const { isLoading } = useAuthContext();
 
-  return;
-  return <>{isLoading ? <p>Loading</p> : <RouterProvider router={router} />}</>;
-};
+//   return;
+//   return <>{isLoading ? <p>Loading</p> : <RouterProvider router={router} />}</>;
+// };
 
 //TODO update route package if they fix https://github.com/TanStack/router/issues/597
 
